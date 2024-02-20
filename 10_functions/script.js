3073574102b28c6bb0f05c9be75a28ba522de720
Joonas Fernando Koskinen <98735275+Fernoss@users.noreply.github.com>
2024-02-20 21:30:19 +0200

    Add files via upload

--- 10_functions/script.js
+++ 10_functions/script.js
@@ -153,4 +153,39 @@ const eurowings = {
   */
  ////////////////////////////////////////////////////////////////////
  // Lecture 5, Bind method
- 
+// Converting this -> book.call(eurowings, 23, 'Sarah Williams');
+// to bind method
+/*
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+
+// also can bind with a parameter set in place
+const bookEW23 = book.bind(eurowings, 23);
+
+bookEW(23, 'Sarah Williams');
+bookEW23('Fernoss Ferrero');
+
+// with eventListners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function() {
+    console.log(this);
+
+    this.planes++
+    console.log(this.planes);
+};
+// lufthansa.buyPlane();
+
+document
+.querySelector('.buy')
+.addEventListener('click', lufthansa
+.buyPlane.bind(lufthansa));
+
+// partial application
+const addTax = (rate, value) => value + value * rate;
+    console.log(addTax(0.1, 200));
+
+// null for 'this.' - setting taxrate parameter 
+const addVAT = addTax.bind(null, 0.24);
+// addVat = value => value + value * 0.24;
+console.log(addVAT(100));
+*/
